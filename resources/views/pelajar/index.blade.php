40f2fd9ef3c86aabb531b759a0dd23c642206658
lizam ali <mohdnoorlizam@gmail.com>
2023-12-09 18:56:55 +0800

    ubah table id

--- resources/views/pelajar/index.blade.php
+++ resources/views/pelajar/index.blade.php
@@ -49,7 +49,7 @@
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body">
-                    <table id="example1" class="table table-bordered table-striped">
+                    <table id="tablePelajar" class="table table-bordered table-striped">
                         <thead>
                             <tr>
                                 <th>Bil</th>
@@ -186,5 +186,6 @@
     </div>
     <!-- /.row -->
 </div>
+
 
 @endsection
