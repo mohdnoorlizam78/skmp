5baaf7f57f43cab6c93c058bd5edcddc878a5f51
lizam ali <mohdnoorlizam@gmail.com>
2023-12-09 21:45:10 +0800

    tukar tabletujuan

--- resources/views/tujuan/index.blade.php
+++ resources/views/tujuan/index.blade.php
@@ -37,7 +37,7 @@
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body">
-                    <table id="example1" class="table table-bordered table-striped">
+                    <table id="tableTujuan" class="table table-bordered table-striped">
                         <thead>
                             <tr>
                                 <th>Bil</th>
